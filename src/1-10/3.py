2ab495b7b337cb058f34f75437a9bae5e2d96d92
dedsinQ <alex.ruhl123@googlemail.com>
2018-11-14 17:49:50 +0100

    3 solved

--- src/1-10/3.py
+++ src/1-10/3.py
@@ -1,13 +1,24 @@
-getPrimeFactors = 600851475143
-largestPrimeFactor = 0
+import math
 
-# exceeds maximum recursion depth
-# def primes(n=2):
-#     yield n
-#     yield from filter(lambda x: x % n, primes(n+1))
-#
-#
-# for p in primes():
-#     if p > (getPrimeFactors / 2):
-#         break
 
+def max_p_factor(n):
+
+    m_p_factor = -1
+
+    while n % 2 == 0:
+        m_p_factor = 2
+        n /= 2
+
+    for i in range(3, int(math.sqrt(n)) + 1, 2):
+        while n % i == 0:
+            m_p_factor = i
+            n /= i
+
+    if n > 2:
+        m_p_factor = n
+
+    return int(m_p_factor)
+
+
+n = 600851475143
+print(max_p_factor(n))
