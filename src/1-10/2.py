087931c2817f5b33cd8a312da0e5dbc9062ce405
dedsinQ <alex.ruhl123@googlemail.com>
2018-11-14 15:01:20 +0100

    2 solved

--- src/1-10/2.py
+++ src/1-10/2.py
@@ -1,28 +1,25 @@
 countN = 4000000
-table = []
-table.append(0)
-table.append(1)
 
-def dynamicFibo(n, table = []):
-    while len(table) < n+1: table.append(0)
-    #base case
-    if n <= 1:
-        return n
-    #recursive case
+
+def sumevenfibonacci(n):
+    a = 0
+    b = 1
+    c = 0
+    sumeven = 0
+    if n < 0:
+        print("Incorrect input")
+    elif n == 0:
+        return 0
+    elif n == 1:
+        return 0
     else:
-        if table[n-1] == 0:
-            table[n-1] = dynamicFibo(n-1)
-
-        if table[n-2] == 0:
-            table[n-2] = dynamicFibo(n-2)
-
-        table[n] = table[n-2] + table[n-2]
-
-    return table[n]
+        while c < n:
+            c = a + b
+            if c % 2 == 0:
+                sumeven += c
+            a = b
+            b = c
+        return sumeven
 
 
-dynamicFibo(countN, table)
-
-
-for i in range(0, len(table)):
-    print(table[i])
+print(sumevenfibonacci(countN))
---
+++ src/1-10/3.py
@@ -0,0 +1,13 @@
+getPrimeFactors = 600851475143
+largestPrimeFactor = 0
+
+# exceeds maximum recursion depth
+# def primes(n=2):
+#     yield n
+#     yield from filter(lambda x: x % n, primes(n+1))
+#
+#
+# for p in primes():
+#     if p > (getPrimeFactors / 2):
+#         break
+
