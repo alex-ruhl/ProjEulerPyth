733b6fcdc063b0831f67bc5d92c739560b21a08d
dedsinQ <alex.ruhl123@googlemail.com>
2018-11-16 16:02:28 +0100

    9 brutus solution

--- src/1-10/9.py
+++ src/1-10/9.py
@@ -1,3 +1,4 @@
+import math
 # A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
 # a2 + b2 = c2
 #
@@ -5,3 +6,16 @@
 #
 # There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 # Find the product abc.
+
+
+def pythagoreantriplet(target):
+
+    for a in range(1, target):
+        for b in range(a, target):
+            for c in range(b, target):
+                if a*a + b*b == c*c:
+                    if a + b + c == target:
+                        return a * b * c
+
+
+print(pythagoreantriplet(1000))
