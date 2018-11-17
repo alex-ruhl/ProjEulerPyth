da8f323174a9a13a41af750d8dfddfbe25337367
dedsinQ <alex.ruhl123@googlemail.com>
2018-11-17 16:47:24 +0100

    11 execution timer added

--- src/11-20/11.py
+++ src/11-20/11.py
@@ -1,3 +1,4 @@
+import time
 # In the 20×20 grid below, four numbers along a diagonal line have been marked in red.
 #
 # The product of these numbers is 26 × 63 × 78 × 14 = 1788696.
@@ -51,7 +52,7 @@ def largestproduct(n, grid):
     return product
 
 
-inp  = [
+inp = [
         [ 8,  2, 22, 97, 38, 15,  0, 40,  0, 75,  4,  5,  7, 78, 52, 12, 50, 77, 91,  8],
         [49, 49, 99, 40, 17, 81, 18, 57, 60, 87, 17, 40, 98, 43, 69, 48,  4, 56, 62,  0],
         [81, 49, 31, 73, 55, 79, 14, 29, 93, 71, 40, 67, 53, 88, 30,  3, 49, 13, 36, 65],
@@ -74,4 +75,6 @@ inp  = [
         [ 1, 70, 54, 71, 83, 51, 54, 69, 16, 92, 33, 48, 61, 43, 52,  1, 89, 19, 67, 48]
        ]
 
+start_time = time.time()
 print(largestproduct(4, inp))
+print("--- %s seconds ---" % (time.time() - start_time))
