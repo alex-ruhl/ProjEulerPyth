bcc0d56266c9ddada30af99b0ef1faefedf8dc65
dedsinQ <alex.ruhl123@googlemail.com>
2018-11-20 13:23:47 +0100

    14 solved, bruteforce

--- src/11-20/14.py
+++ src/11-20/14.py
@@ -12,3 +12,27 @@
 # Which starting number, under one million, produces the longest chain?
 #
 # NOTE: Once the chain starts the terms are allowed to go above one million.
+
+
+def collatzsequence(start):
+    count = 1
+    while start != 1:
+        if start % 2 == 0:
+            start //= 2
+        else:
+            start = 3 * start + 1
+        count += 1
+    return count
+
+
+def findlongest(maxval):
+    longest = 0
+    startnumber = 0
+    for i in range(1, maxval):
+        if longest < collatzsequence(i):
+            startnumber = i
+            longest = collatzsequence(i)
+    return startnumber
+
+
+print(findlongest(1000000))
